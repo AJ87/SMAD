fc392f147a6334119ddf09bef8671eae2fddceba
AJ87 <ultimateaj87@gmail.com>
2015-12-07 23:47:45 +1100

    Refactor of javascript class to remove global variables

--- app.js
+++ app.js
@@ -1,71 +1,86 @@
-var oldMenuItem;
+"use strict"
+
+function app() {
+  var oldMenuItem, child;
+
+  function factory() {
+    var children = 1;
+    return {
+      add: function(){
+        children = children + 1;
+      },
+      get: function(){
+        return children;
+      },
+      remove: function(){
+        children = children - 1;
+      }};
+  }
+
+  child = factory();
+
+  function showHide(id, vis) {
+    var actions = {
+      'contactMenu': function(vis) {
+        document.getElementById('contact').style.display = vis;
+      },
+      'emergMenu': function(vis) {
+        document.getElementById('emergency').style.display = vis;
+      },
+      'childMenu': function(vis) {
+        document.getElementById('child').style.display = vis;
+      },
+      'consentMenu': function(vis) {
+        document.getElementById('consent').style.display = vis;
+      }
+    }
+    return actions[id](vis);
+  }
+
+  return {
+    popup: function(url) {
+      var popup=window.open(url,'Conditions','height=300,width=500,scrollbars=1,resizable=1,toolbar=1');
+      if (window.focus) {popup.focus()}
+    },
+    addChild: function(button) {
+      child.add();
+      document.getElementById('child' + child.get()).style.display = 'block';
+      document.getElementById('btnChild').style.display = 'block';
+      document.getElementById('btnRemove').style.display = 'block';
+      window.location.href = window.location.pathname + '#child' + child.get();
+      if (child.get() === 4) {
+        button.style.display = 'none';
+      };
+    },
+    removeChild: function(button) {
+      document.getElementById('child' + child.get()).style.display = 'none';
+      child.remove();
+      document.getElementById('btnChild').style.display = 'block';
+      document.getElementById('btnRemove').style.display = 'block';
+      window.location.href = window.location.pathname + '#child' + child.get();
+      if (child.get() === 1) {
+        button.style.display = 'none';
+      };
+    },
+    menuSelect: function(elem) {
+      oldMenuItem.className = 'not';
+      elem.className = 'active';
+
+      showHide(oldMenuItem.id, 'none');
+
+      oldMenuItem = elem;
+
+      showHide(elem.id, 'block');
+    },
+    setup: function() {
+      oldMenuItem = document.getElementById('contactMenu');
+      showHide(oldMenuItem.id, 'block');
+    }
+  };
+}
+
+var app = app();
 
 window.onload = function(){
-  oldMenuItem = document.getElementById('contactMenu');
-  showHide(oldMenuItem.id, 'block');
+  app.setup();
 };
-
-function popup(url)
-{
- var popup=window.open(url,'Conditions','height=300,width=500,scrollbars=1,resizable=1,toolbar=1');
- if (window.focus) {popup.focus()}
-}
-
-function factory() {
-  var children = 1;
-  return {
-    add: function(){
-      children = children + 1;
-    },
-    get: function(){
-      return children;
-    }};
-}
-
-var child = factory();
-
-function addChild(button) {
-  child.add();
-  document.getElementById('child' + child.get()).style.display = 'block';
-  if (child.get() === 4) {
-    button.style.display = 'none';
-  }
-}
-
-function menuSelect(elem){
-  oldMenuItem.className = 'not';
-  elem.className = 'active';
-
-  showHide(oldMenuItem.id, 'none');
-
-  oldMenuItem = elem;
-
-  showHide(elem.id, 'block');
-}
-
-function showHide(id, vis){
-  var actions = {
-    'contactMenu': function(vis) {
-        document.getElementById('contact').style.display = vis;
-    },
-    'emergMenu': function(vis) {
-        document.getElementById('emergency').style.display = vis;
-    },
-    'childMenu': function(vis) {
-        document.getElementById('child').style.display = vis;
-    },
-    'consentMenu': function(vis) {
-        document.getElementById('consent').style.display = vis;
-    }
-/*    '2': function(vis) {
-        document.getElementById('child2').style.display = vis;
-    },
-    '3': function(vis) {
-        document.getElementById('child3').style.display = vis;
-    },
-    '4': function(vis) {
-        document.getElementById('child4').style.display = vis;
-    }*/
-  }
-  return actions[id](vis);
-}
